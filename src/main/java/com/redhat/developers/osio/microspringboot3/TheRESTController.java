6ab973af909d4e3ff943ebc2512d82bbe78d3b31
burrsutter <burrsutter@gmail.com>
2017-08-28 21:22:02 -0400

    added POST

--- src/main/java/com/redhat/developers/osio/microspringboot3/TheRESTController.java
+++ src/main/java/com/redhat/developers/osio/microspringboot3/TheRESTController.java
@@ -2,10 +2,11 @@ package com.redhat.developers.osio.microspringboot3;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class TheRESTController {
-    @RequestMapping("/api/orders") 
+    @RequestMapping(value ="/api/orders", method = RequestMethod.POST) 
     public String getOrders(String customers) {
         return "O4321,O5432,O6543";
     }
